a95828a9b34806e47e713bcba6c180946ac496b5
Yash Tulsyan <yashtulsyan@gmail.com>
2012-02-23 17:46:33 -0800

    Changed players-at function

--- structs.lisp
+++ structs.lisp
@@ -23,18 +23,17 @@ You should have received a copy of the GNU General Public License
 (defparameter *rooms* (make-hash-table))
 
 ;To prevent bugs with players and objects, create a "build-room" function that takes a name, setfs a var to the room, calls players-at and objects-at to define the objects and players....
-;;The following chunk of code was something I made naïvely. It won't worķ and for good reason, too.
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;(defun players-at-aux (location-name) (defparameter #'(players-at-aux-1 location-name) #'(list nil))) ;This needs an aux function itself due to being a list....Ugly, I know....
-;(defun players-at-aux-1 (location-name) (concatenate 'string "players-" location-name))
-;(defun players-at (name-room list-players)
-;  (dolist (players-i (mapcar #'username-variable *registered-usernames*))
-;    (if (equalp (player-location players-i) name-room)
-;	(cons players-i (players-at-aux-1 name-room)))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;Edit: above comment will no longer be needed
 (defun players-at ()
-  (labels ((players-at-indiv ()
-	     
+  (let ((player-list ()))
+    (labels ((players-at-indiv (room)
+	       (maphash #'(lambda (players-i) 
+			    (if (equalp (player-location players-i) (locale-name room))
+				(cons players-i player-list))) *users*)
+	       (setf (locale-players room) player-list)))
+      (maphash #'players-at-indiv *rooms*))))
+      
+
 ;Objects-at will probably have a cleaner design--I'll make an alist of objects and their locations, so it'll be easier to go through it...
 (defstruct player name password gender species health mana level experience stream location saved-location con cha cre str dex inventory) ;Set some default values for these, maybe change to defclass. ALSO: make int wis str dex cha etc....
 ;Also, write a function that dolists through the players and if their experience is above, say, 100, then incf their level and setf the experience to their experience minus 100. Have this running at all times.
